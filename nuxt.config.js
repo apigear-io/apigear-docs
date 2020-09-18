71ea17ae6cb98ac42d45a6401d88a226acc81d56
Wolfgang Bremer <wolfgang@w-bremer.de>
2020-09-18 15:43:39 +0200

    Re-enable google anayltics using google tag manager
    Google tag manager is default disabled when run in dev mode.
    It will be active in staging/production.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,18 @@
 import theme from "@nuxt/content-theme-docs";
 
-export default theme();
+export default theme({
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    '@nuxtjs/gtm'
+  ],
+  /*
+   ** Google Tag Maanger configuration
+   ** See https://github.com/nuxt-community/gtm-module
+   */
+  gtm: {
+    id: 'GTM-M336GFQ',
+    pageTracking: true
+  }
+});
