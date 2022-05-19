6fd36b9a4d2b420447a12cf9d0ccdf3bfcbe07b1
Wolfgang Bremer <wbremer@apigear.io>
2022-05-19 17:43:18 +0200

    build: Add Analytics support

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -146,6 +146,12 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
-    'vuepress-plugin-mermaidjs'
+    'vuepress-plugin-mermaidjs',
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-166716652-4'
+      }
+    ]
   ]
 }
