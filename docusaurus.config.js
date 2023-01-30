ed29c8cc7d4b96c023068b898a4dd7f20f9da29a
Jürgen Ryannel <juergen@ryannel.org>
2023-01-30 19:27:01 +0100

    add gtag id for apigear

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-KD8KC8Z417',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
